07342e918c13b548cbbded8df4f0d6439eccba88
TuxDave <godinodavide@gmail.com>
2021-01-30 18:26:30 +0100

    init commit

---
+++ src/main/java/com/tuxdave/PianoTailCheat/Main.java
@@ -0,0 +1,8 @@
+package com.tuxdave.PianoTailCheat;
+
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/test/java/Test1.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+import java.util.Random;
+
+public class Test1 {
+    public static final int FIVE_SECONDS = 3000;
+    public static final int MAX_X = 1920;
+    public static final int MAX_Y = 1080;
+
+    public static void main(String... args) throws Exception {
+        Robot robot = new Robot();
+        Random random = new Random();
+        while (true) {
+            robot.mouseMove(random.nextInt(MAX_X), random.nextInt(MAX_Y));
+            System.out.println(MouseInfo.getPointerInfo().getLocation());
+//            robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+//            robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+            Thread.sleep(FIVE_SECONDS);
+        }
+    }
+}
